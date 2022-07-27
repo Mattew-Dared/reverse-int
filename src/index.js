8fa9193e5a3c77f09c5dc32bd5e1a8b928129dec
Mattew-Dared <dd96ua@gmail.com>
2022-07-27 19:17:58 +0300

    feat: add solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    // перетворили задане число в рядок, нарізали із рядка масив рядків (кожна цифра окремий елемент), перевернули усі елементи всередині масиву, обєднали усі елементи масива у один рядок, зберегли результат у змінній
+    let reverseInt = n.toString().split("").reverse().join("");
+    return parseInt(reverseInt);
+};
